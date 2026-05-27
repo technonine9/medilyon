cbf0ca8ce8fe77f7f5fa044b01081476d9bac5b7
technonine9 <Sahith2@hotmail.com>
2026-05-27 11:36:12 -0500

    Delete medilyon-nextjs directory

--- medilyon-nextjs/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'cdn.sanity.io' },
-    ],
-  },
-};
-module.exports = nextConfig;
